8077d0f50d1537fd08725ee1a5ba3f6cc1f2b23e
Laysa Souza <karollinylaysa@gmail.com>
2024-05-21 10:21:53 +0100

    Added security configuration

---
+++ src/main/java/com/netflix/webapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.netflix.webapp.services;
+
+import com.netflix.webapp.entities.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.netflix.webapp.repositories.UserRepository;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findOneByUsername(username);
+
+        if (user != null) {
+            return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+        } else {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<String> roles) {
+        return roles.stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+            .collect(Collectors.toList());
+    }
+}
